fb5917dc19ed664eeae7a64a41a3ac483f3285a1
Rahul Sharma <rahulsharma02121998@gmail.com>
2023-01-05 07:03:17 +0530

    footer is implemented

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Footer from "../common/Footer";
 import GlobalLoading from "../common/GlobalLoading";
+import Topbar from "../common/Topbar";
 
 const MainLayout = () => {
   return (
@@ -14,6 +16,7 @@ const MainLayout = () => {
 
       <Box display="flex" minHeight="100vh">
         {/* header */}
+        <Topbar />
         {/* header */}
 
         {/* main */}
@@ -22,6 +25,9 @@ const MainLayout = () => {
         </Box>
         {/* main */}
       </Box>
+      {/* footer */}
+      <Footer />
+      {/* footer */}
     </>
   );
 };
