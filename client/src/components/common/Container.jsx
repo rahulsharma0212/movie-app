541ddbce19ae3e867b0bbc8ed8b339e265ec283c
Rahul Sharma <rahulsharma02121998@gmail.com>
2023-01-14 21:42:56 +0530

    movie slider is completed

--- client/src/components/common/Container.jsx
+++ client/src/components/common/Container.jsx
@@ -29,7 +29,7 @@ const Container = ({ header, children }) => {
               },
             }}
           >
-            <Typography variant="h5" fontWeight="700">
+            <Typography variant="h5" fontWeight="700" textTransform="uppercase">
               {header}
             </Typography>
           </Box>
